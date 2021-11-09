330ee1fecd5f364c41b605b906e1a23f6ddfe31d
Mahir Ahmed <mahir.ahmed56@hotmail.com>
2021-11-09 14:39:54 -0500

    new redirect with randomly generated shortURL

--- express_server.js
+++ express_server.js
@@ -3,10 +3,19 @@ const app = express();
 const PORT = 8080; // default port 8080
 app.set("view engine", "ejs");
 
+function generateRandomString()  {
+  return Math.random().toString(36).slice(2);
+}
+
+
+
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -23,11 +32,29 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 app.get("/urls", (req, res) => {
-  const templateVars = { 
+  const templateVars = {
     urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("urls_show", templateVars);
 });
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  const shortUrl = generateRandomString()  // creating a new shortUrl using the new function 
+  const longURL = req.body.longURL // the new longUrl is coming from the form submission
+  urlDatabase[shortUrl] = longURL // add new shortUrl to the urlDatabase object
+  res.redirect(`/urls/${shortUrl}`);
+});
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL]
+  res.redirect(longURL);
+});
+
+
+
+
